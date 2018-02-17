2b19929a24da75fe35581bf304b94bbd6f72b28b
Michael Xiao <michaelxiao16@gmail.com>
2018-02-17 15:07:45 -0500

    Registration continued updated

--- app/src/main/java/weboolean/weboolean/RegistrationActivity.java
+++ app/src/main/java/weboolean/weboolean/RegistrationActivity.java
@@ -2,19 +2,35 @@ package weboolean.weboolean;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.FirebaseUser;
 
 public class RegistrationActivity extends AppCompatActivity {
-
+    private FirebaseAuth mAuth;
+    //tag for logcat
+    public static final String TAG = RegistrationActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        mAuth = FirebaseAuth.getInstance();
+        //auto generated code for floating action button (email button)
 //        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 //        setSupportActionBar(toolbar);
 //
@@ -26,18 +42,53 @@ public class RegistrationActivity extends AppCompatActivity {
 //                        .setAction("Action", null).show();
 //            }
 //        });
-        final Button logoutButton = findViewById(R.id.LogOutButton);
-
-        logoutButton.setOnClickListener(new View.OnClickListener() {
+        final Button registerButton = findViewById(R.id.Registration_RegisterButton);
+        registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                logOut();
+                attemptRegistration();
             }
         });
     }
-    private void logOut() {
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        //check to see if user is currently signed in! Update ui accordingly
+        //updateUI(currentUser);
     }
 
+    private void attemptRegistration() {
+        String userEmail = ((EditText) findViewById(R.id.Registration_Email)).getText().toString();
+        String userPassword = ((EditText) findViewById(R.id.Registration_Password)).getText().toString();
+
+        if (userEmail.equals("") || userPassword.equals("")) {
+            Toast.makeText(this, "Must Provide Values", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(this, "Valid Values", Toast.LENGTH_LONG).show();
+            mAuth.createUserWithEmailAndPassword(userEmail, userPassword)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Log.d(TAG, "createUserWithEmail:success");
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                //updateUI(user);
+                                //Switch to logged in screen
+                                Intent intent = new Intent(RegistrationActivity.this, WelcomeActivity.class);
+                                startActivity(intent);
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                                Toast.makeText(RegistrationActivity.this, "Authentication failed.",
+                                        Toast.LENGTH_SHORT).show();
+                                //updateUI(null);
+                            }
+                        }
+                    });
+
+
+
+        }
+    }
 }
