0e0bcdea9158b3a278b5aefd2a189c80fba183dc
Michael Xiao <michaelxiao16@gmail.com>
2018-02-17 15:43:24 -0500

    Implemented sign in with firebase

--- app/src/main/java/weboolean/weboolean/LoginActivity.java
+++ app/src/main/java/weboolean/weboolean/LoginActivity.java
@@ -2,6 +2,7 @@ package weboolean.weboolean;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -12,18 +13,28 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 import java.util.HashMap;
 
 public class LoginActivity extends AppCompatActivity {
 
     private static HashMap<String, String> accounts = new HashMap<>();
+    private FirebaseAuth mAuth;
+    //tag for logcat
+    public static final String TAG = LoginActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        accounts.put("user", "pass");
+        //accounts.put("user", "pass");
+        mAuth = FirebaseAuth.getInstance();
 
         final Button signInButton = findViewById(R.id.sign_in_button);
         signInButton.setOnClickListener(new View.OnClickListener() {
@@ -40,9 +51,14 @@ public class LoginActivity extends AppCompatActivity {
                 cancelLogin();
             }
         });
+    }
 
-
-
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        //updateUI(currentUser);
     }
 
     private void cancelLogin() {
@@ -53,16 +69,38 @@ public class LoginActivity extends AppCompatActivity {
     private void attemptedLogin() {
         String userEmail = ((EditText) findViewById(R.id.email)).getText().toString();
         String userPassword = ((EditText) findViewById(R.id.password)).getText().toString();
+        mAuth.signInWithEmailAndPassword(userEmail, userPassword)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            //updateUI(user);
+                            Toast.makeText(LoginActivity.this, "Successful Login", Toast.LENGTH_LONG).show();
+                            Intent intent = new Intent(LoginActivity.this, WelcomeActivity.class);
+                            startActivity(intent);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, "Login failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            //updateUI(null);
+                        }
 
-        if (userEmail.equals("") || userPassword.equals("")) {
-            Toast.makeText(this, "Must Provide Values", Toast.LENGTH_LONG).show();
-        } else if (userPassword.equals(accounts.get(userEmail))) {
-            Toast.makeText(this, "Successful Login", Toast.LENGTH_LONG).show();
-            Intent intent = new Intent(this, WelcomeActivity.class);
-            startActivity(intent);
-        } else {
-            Toast.makeText(this, "Wrong Credentials ", Toast.LENGTH_LONG).show();
-        }
+                        // ...
+                    }
+                });
+//        if (userEmail.equals("") || userPassword.equals("")) {
+//            Toast.makeText(this, "Must Provide Values", Toast.LENGTH_LONG).show();
+//        } else if (userPassword.equals(accounts.get(userEmail))) {
+//            Toast.makeText(this, "Successful Login", Toast.LENGTH_LONG).show();
+//            Intent intent = new Intent(this, WelcomeActivity.class);
+//            startActivity(intent);
+//        } else {
+//            Toast.makeText(this, "Wrong Credentials ", Toast.LENGTH_LONG).show();
+//        }
     }
 }
 
