78470cb1767afddbabf753170510e2f48d1faf4a
Raj Shrimali <rajshrimali@gmail.com>
2018-02-28 18:11:56 -0500

    documentation for singleton

--- app/src/main/java/weboolean/weboolean/ShelterSingleton.java
+++ app/src/main/java/weboolean/weboolean/ShelterSingleton.java
@@ -20,12 +20,29 @@ import weboolean.weboolean.models.Shelter;
 
 public class ShelterSingleton implements Runnable {
 
+    //This is going to hold our copy of the shelters
     private static ArrayList<Shelter> shelters = new ArrayList<>();
+
+    //firebase stuff
     private static FirebaseDatabase db;
     private static DatabaseReference reference;
+
+    //Make sure only one copy of this is ever instantiated.
+    //Ideally, we find some better way that deletes this on deletion but whatever.
     private static boolean instantiated = false;
+
+    //Logging. Currently unused.
     public static final String TAG = Shelter.class.getSimpleName();
+
+    //Mutex lock. Ensures we never try to write to the shelter array while someone is getting
+    //a copy of it. Bad things could happen otherwise.
     private static final Lock mutexloc = new ReentrantLock();
+
+    /** Creates our first shelter.
+     * Upon it already being instantiated, it throws an instantiation exception.
+     * If you do everything right and behave, it'll never happen ;)
+     * @throws InstantiationException multiple instantiation
+     */
     public ShelterSingleton() throws InstantiationException {
         if (ShelterSingleton.instantiated) {
             throw new InstantiationException("Only one ShelterSingleton instance allowed");
@@ -34,25 +51,37 @@ public class ShelterSingleton implements Runnable {
             ShelterSingleton.instantiated = true;
         }
     }
+
     @Override
     public void run() {
+        //initialize our connection.
         db = FirebaseDatabase.getInstance();
         reference = db.getReference("shelters");
+
+        //Add a permanent listener to our reference.
         reference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
+                //Go through our data update
                 for (DataSnapshot singleSnapshot : dataSnapshot.getChildren()) {
+                    //get our individual shelter.
                     Shelter s = singleSnapshot.getValue(Shelter.class);
+
+                    //This code here updates every shelter, making sure the keys match up.
                     boolean found = false;
                     for (int i = 0 ; i < ShelterSingleton.shelters.size(); i++) {
                         if (ShelterSingleton.shelters.get(i).equals(s)) {
+
+                            //Lock the lock when updating
                             mutexloc.lock();
                             ShelterSingleton.shelters.set(i, s); //update shelter at current point
+                            //and release instantly.
                             mutexloc.unlock();
                             found = true;
                             break;
                         }
                     }
+                    //add it to the list if not found.
                     if (!found) {
                         ShelterSingleton.shelters.add(s);
                     }
