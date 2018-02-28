e586d32a3fcdece93783e84fdaec66103717721c
Michael Xiao <michaelxiao16@gmail.com>
2018-02-28 17:32:03 -0500

    Continued updates to welcomeactivity

--- app/src/main/java/weboolean/weboolean/WelcomeActivity.java
+++ app/src/main/java/weboolean/weboolean/WelcomeActivity.java
@@ -3,6 +3,7 @@ package weboolean.weboolean;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -11,20 +12,25 @@ import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+
+import weboolean.weboolean.models.Shelter;
 
 public class WelcomeActivity extends AppCompatActivity {
     ArrayList<String> listItems = new ArrayList<String>();
     ArrayAdapter<String> adapter;
     private ListView mListView;
-
+    public static final String TAG = WelcomeActivity.class.getSimpleName();
 
     FirebaseDatabase mDatabase = FirebaseDatabase.getInstance();
-    DatabaseReference ref = database.getReference("shelters");
+    DatabaseReference ref = mDatabase.getReference("shelters");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,15 +49,20 @@ public class WelcomeActivity extends AppCompatActivity {
 
 
 
-        Query shelterQuery = mDatabase.orderByChild("shelters").equalTo(0);
-        shelterQuery.addListenerForSingleValueEvent(new ValueEventListener() {
+        ref.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 for (DataSnapshot singleSnapshot : dataSnapshot.getChildren()) {
-                    UserType t = singleSnapshot.getValue(UserType.class);
-                    CurrentUser.setUserInstance(new User(user.getUid(), t), user);
+                    Shelter s = singleSnapshot.getValue(Shelter.class);
+                    Log.d(TAG, s.toString());
                 }
             }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                System.out.println("The read failed: " + databaseError.getCode());
+            }
+        });
         if (mListView == null) {
             mListView = findViewById(R.id.list);
         }
