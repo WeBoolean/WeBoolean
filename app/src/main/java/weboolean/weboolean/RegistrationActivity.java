4af53ee8ceb3384a3763bd2dd76497f3b5e20a41
Raj Shrimali <rajshrimali@gmail.com>
2018-03-29 17:48:30 -0400

    API key instructions

--- app/src/main/java/weboolean/weboolean/RegistrationActivity.java
+++ app/src/main/java/weboolean/weboolean/RegistrationActivity.java
@@ -130,8 +130,10 @@ public class RegistrationActivity extends AppCompatActivity {
                         mDatabase.child("users").child(user.getUid())
                                 .setValue(u);
 
-                        //Launch login activity
-g                        startActivity(intent);
+                        //Launch login activit
+                        Intent intent = new Intent(RegistrationActivity.this, MapsActivity.class);
+                        startActivity(intent);
+
                     } else {
                         // If sign in fails, display a message to the user.
                         Log.w(TAG, "createUserWithEmail:failure", task.getException());
