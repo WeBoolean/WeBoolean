f3a5765fe515d14ed0e75578e1e8cb72c5732a34
mxiao32 <michaelxiao16@gmail.com>
2018-04-11 17:05:34 -0400

    implicitly used xml

--- app/src/main/java/weboolean/weboolean/ShelterActivity.java
+++ app/src/main/java/weboolean/weboolean/ShelterActivity.java
@@ -9,8 +9,6 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.firebase.database.FirebaseDatabase;
-
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
